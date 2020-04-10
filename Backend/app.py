f890dcbc47f63fc015315dc69a8e33def7c5c5f0
Arpitsinh Vaghela <arpitvaghela9210@gmail.com>
2020-04-10 22:49:31 +0530

    test backed

---
+++ Backend/app.py
@@ -0,0 +1,43 @@
+# Imports
+from flask import Flask
+from flask_graphql import GraphQLView
+import query as q
+# initializing our app
+app = Flask(__name__)
+app.debug = True
+
+from graphene import ObjectType, String, Schema
+
+
+class Query(ObjectType):
+    # this defines a Field `hello` in our Schema with a single Argument `name`
+    hello = String(name=String(default_value="stranger"))
+    goodbye = String()
+
+    # our Resolver method takes the GraphQL context (root, info) as well as
+    # Argument (name) for the Field and returns data for the query Response
+    def resolve_hello(root, info):
+        return f'Hello {name}!'
+
+    def resolve_goodbye(root, info):
+        return q.allUsers()
+
+
+schema = Schema(query=Query)
+
+
+@app.route('/')
+def index():
+    return 'Welcome to Api2.0'
+
+
+app.add_url_rule(
+    '/graphql-api',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True  # for having the GraphiQL interface
+    ))
+
+if __name__ == '__main__':
+    app.run()
---
+++ Backend/query.py
@@ -0,0 +1,51 @@
+import psycopg2 as db
+
+DSN = "dbname='pms' user='arpit' host='localhost' password='1let2me3in'"
+# establish connection
+
+
+def createUser(username: str,
+               fisrstname: str,
+               lastname: str,
+               password: str,
+               email: str,
+               profilepic=None):
+    sql = "INSERT INTO users VALUES (%s,%s,%s,%s,%s,%s)"
+    data = [username, fisrstname, lastname, password, email, profilepic]
+    try:
+        with db.connect(DSN) as conn:
+
+            try:
+                with conn.cursor() as cur:
+                    cur.execute(sql, data)
+                    print(cur.query)
+
+            except:
+                print("failed to add user")
+
+    except db.OperationalError:
+        print("connection failed")
+
+
+def allUsers():
+    sql = "SELECT * from users"
+    try:
+        with db.connect(DSN) as conn:
+
+            try:
+                with conn.cursor() as cur:
+                    cur.execute(sql)
+                    print(cur.query)
+                    rows = cur.fetchall()
+                    return rows
+            except:
+                print("failed to add user")
+
+    except db.OperationalError:
+        print("connection failed")
+
+
+if __name__ == "__main__":
+    #createUser('_$arpit2', 'Arpit', 'Vaghela', '', 'dhruvildave@ahduni.edu.in')
+    print(allUsers())
+te

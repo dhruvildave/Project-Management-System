63b5fb58b601178d3e95174d1f85d7deb6eb222d
Kaushal1011 <kaushalpatil10@gmail.com>
2020-04-19 18:27:14 +0530

    Reports done

---
+++ project_manager/src/components/PieChart.jsx
@@ -0,0 +1,42 @@
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Cell } from "recharts";
+
+const data = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+  { name: "Group E", value: 100 },
+];
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#AF8F42",
+  "FE0349",
+];
+
+export default class Example extends PureComponent {
+  render() {
+    return (
+      <PieChart width={400} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={this.props.data}
+          cx={200}
+          cy={200}
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+          label={true}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    );
+  }
+}
